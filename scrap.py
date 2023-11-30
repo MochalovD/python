585889791ad49dfcc436b26313995cb8e6784a48
Dmitrii Mochalov <jpdomitori@gmail.com>
2023-11-30 22:05:08 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,10 +1,6 @@
 import requests
-#import telegram
 from bs4 import BeautifulSoup
-#from discordwebhook import Discord
 
-#bot = telegram.Bot(token='6420236709:AAENJdnAqASRZHbBkiZYwKBoqIaZFXmh6b4')
-#channel_id = '1001971699769'
 response = requests.get("https://gamemag.ru/")
 soup = BeautifulSoup(response.content, "html.parser")
 
@@ -13,10 +9,7 @@ url = []
 for data in soup.find_all('div', class_='news-item__text-wrapper'):
     for a in data.find_all('a'):
         url.append(a.get('href'))
-
-#discord = Discord(url="https://discord.com/api/webhooks/1178117663747227728/Bn4yzvS0mi4el6GBIlv6KSjLfGFeyPEPGS7JX5bUQa2AQGqYyFXPr9-h0IgvlrZQrxc8")
-#discord.post(content="first_message")
-
+        
 testmessage = f"https://gamemag.ru{url[0]}\n" + f"https://gamemag.ru{url[1]}\n" + f"https://gamemag.ru{url[2]}\n" + f"https://gamemag.ru{url[3]}\n" + f"https://gamemag.ru{url[4]}"
 
 mUrl = "https://discord.com/api/webhooks/1178117663747227728/Bn4yzvS0mi4el6GBIlv6KSjLfGFeyPEPGS7JX5bUQa2AQGqYyFXPr9-h0IgvlrZQrxc8"
